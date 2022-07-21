14767398576de91826fdf73fba98c6b1f5d4362a
root <harshalmahapure44@gmail.com>
2022-07-21 14:01:03 +0000

    cycle in linkList

--- linklist/cycle.cpp
+++ linklist/cycle.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "linkList.h"
+#include <map>
+using namespace std;
 
+
+ListNode * giveCycleNode(ListNode *head){
+    map<ListNode*,bool> mp;
+
+    auto temp = head;
+    while(temp){
+       if(mp.find(temp) != mp.end())
+           return temp;
+       mp[temp] = true;
+       temp = temp->next;
+    }
+
+    return NULL;
+}
+
+
+int  main(){
+
+    return 0;
+}
