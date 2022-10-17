c44af016f2d4fc3ee7adf063f143c71e3cd0a4a2
* <harshalmahapure44@gmail.com>
2022-10-17 07:50:05 +0530

    peak element array qiertion

---
+++ array/peakElement.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int findPeak(int arr[],int n){
+   if(arr[0] >= arr[1] || n==1){
+	return 0;
+   }
+   if(arr[n-1] >= arr[n-2]){
+	   return n-1;
+   }
+
+
+   for(int i=1;i<n;i++){
+	if(arr[i] >= arr[i-1] && arr[i] >=  arr[i+1])
+	    return i;
+   }
+
+   return 0;
+}
+
+int main(){
+   int arr[] = { 1, 3, 20, 4, 1, 0 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Index of a peak point is " << findPeak(arr, n);
+    return 0;
+}
